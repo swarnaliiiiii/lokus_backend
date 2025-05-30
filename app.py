7cfdc7bf29a6ae62bbaf9161d67a72a696ba6c04
swarnaliiiiii <oli2k3@gmail.com>
2025-05-30 20:47:27 +0530

    Add .env file and reorder imports in app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import uuid
 import logging
+from flask_sqlalchemy import SQLAlchemy
+
+
